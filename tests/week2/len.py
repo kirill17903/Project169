9a990f1eb5d7e54d7083b33011f0737fa5cf6f4d
kirill17903 <kv.kirill2000@gmail.com>
2026-08-06 22:29:45 +0300

    feat: add week2 python practice (oop, loops, match-case)

--- tests/week2/len.py
+++ tests/week2/len.py
@@ -4,3 +4,5 @@ def filter_long(words):
     return [word for word in words if len(word) > 5]
 
 print(filter_long(words))
+
+
---
+++ tests/week2/while_for.py
@@ -0,0 +1,8 @@
+numbers = [4,14,525,2,0,10,77,534,44]
+max_number = numbers[0]
+for num in numbers:
+    if num > max_number:
+        max_number = num
+
+print(f'Максимум: {max_number}')
+
