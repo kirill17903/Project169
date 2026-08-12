e0202f1794661acff4b2749e774a60c7356b9c7b
kirill17903 <kv.kirill2000@gmail.com>
2026-08-12 20:13:07 +0300

    chore: keep only week1 files in this branch

--- tests/week2/len.py
+++
@@ -1,8 +0,0 @@
-words = ["cat", "python", "hi", "automation", "dog", "testing"]
-
-def filter_long(words):
-    return [word for word in words if len(word) > 5]
-
-print(filter_long(words))
-
-
--- tests/week3/test_shared_fixture.py
+++
@@ -1,6 +0,0 @@
-def test_name(sample_data):
-    assert sample_data["name"] == "test_user"
-
-
-def test_age(sample_data):
-    assert sample_data["age"] == 25
